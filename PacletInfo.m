9435b2665fc28b356db83ecc2301124191ad3b03
Barry Wardell <barry.wardell@gmail.com>
2012-09-25 16:27:45 -0400

    Add PacletInfo.m

---
+++ PacletInfo.m
@@ -0,0 +1,18 @@
+(* Paclet Info File *)
+
+(* created 2011/03/05*)
+
+Paclet[
+    Name -> "CovariantSeries",
+    Version -> "0.0.1",
+    MathematicaVersion -> "6+",
+    Creator -> "Barry Wardell",
+    Extensions -> 
+        {
+            {"Documentation", Resources -> 
+                {"Guides/CovariantSeries Guide"}
+            , Language -> "English"}
+        }
+]
+
+
